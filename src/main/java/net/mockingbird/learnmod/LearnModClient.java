09426182912caa6e2cda9f505effed175a860d36
KonSit <konsitness@gmail.com>
2023-11-30 21:51:10 +0300

    Initial commit

---
+++ src/main/java/net/mockingbird/learnmod/LearnModClient.java
@@ -0,0 +1,10 @@
+package net.mockingbird.learnmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearnModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
